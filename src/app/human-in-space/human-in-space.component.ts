b636e86e5c10b2cd81f8b2276d310f06efe367a4
sgglko <jonas.siggelkow@unitybox.de>
2023-03-30 22:17:08 +0200

    added first try with http service

--- src/app/human-in-space/human-in-space.component.ts
+++ src/app/human-in-space/human-in-space.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import { HttpService } from '../http.service';
 
+interface Data {
+  message: string;
+  amountOfPeople: number;
+  dataPeople: string[];
+
+}
 @Component({
   selector: 'app-human-in-space',
   templateUrl: './human-in-space.component.html',
   styleUrls: ['./human-in-space.component.scss']
 })
+
+
 export class HumanInSpaceComponent {
+  Data: any = [];
+  constructor(private httpService: HttpService) {}
+
+  apiCall() {
+    this.httpService.getRequest('http://api.open-notify.org/astros.json').subscribe(data => {
+      console.log(data);
+      this.Data = data;
+    });
+  }
 
 }
+
