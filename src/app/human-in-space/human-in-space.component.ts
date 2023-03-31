325d0b48498a353303ef6d4a9da2a2a3333cfb60
sgglko <jonas.siggelkow@unitybox.de>
2023-03-31 12:34:24 +0200

    added people in space finished

--- src/app/human-in-space/human-in-space.component.ts
+++ src/app/human-in-space/human-in-space.component.ts
@@ -1,12 +1,6 @@
-import {Component, Inject} from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import { HttpService } from '../http.service';
 
-interface Data {
-  message: string;
-  amountOfPeople: number;
-  dataPeople: string[];
-
-}
 @Component({
   selector: 'app-human-in-space',
   templateUrl: './human-in-space.component.html',
@@ -15,15 +9,65 @@ interface Data {
 
 
 export class HumanInSpaceComponent {
-  Data: any = [];
-  constructor(private httpService: HttpService) {}
+  message?: string;
+  amountOfPeople?: number;
+  dataPeople?: { craft: string, name: string }[];
+  dataISS?: { name: string }[];
+  dataShenzhou15?: { name: string }[];
+  buttonClicked: boolean = false;
 
-  apiCall() {
-    this.httpService.getRequest('http://api.open-notify.org/astros.json').subscribe(data => {
-      console.log(data);
-      this.Data = data;
-    });
+  constructor(private httpService: HttpService) {
   }
 
-}
+  showPeople() {
+    this.httpService.getRequest('http://api.open-notify.org/astros.json').subscribe((data) => {
+      console.log(data);
+      this.message = data.message;
+      this.dataPeople = data.people;
+      this.amountOfPeople = data.number;
+      console.log(this.dataPeople);
+      console.log(this.amountOfPeople);
+      console.log(this.message)
+      this.sortPeopleByCraft()
+    });
 
+
+  }
+
+  sortPeopleByCraft() {
+
+    this.dataShenzhou15?.splice(0);
+    this.dataISS?.splice(0);
+
+    if (!this.dataShenzhou15) {
+      this.dataShenzhou15 = [];
+    }
+    if (!this.dataISS) {
+      this.dataISS = [];
+    }
+
+    for (let i = 0; i < this.dataPeople!.length; i++) {
+      if (this.dataPeople![i].craft === 'Shenzhou 15') {
+        this.dataShenzhou15.push(this.dataPeople![i]);
+        this.dataShenzhou15.sort((a, b) => a.name.localeCompare(b.name))
+      } else if (this.dataPeople![i].craft === 'ISS') {
+        this.dataISS.push(this.dataPeople![i]);
+        this.dataISS.sort((a, b) => a.name.localeCompare(b.name))
+      }
+    }
+  }
+
+  rocketStart() {
+    const element = document.getElementById('button');
+
+    element?.classList.add('rocketStart');
+
+    element?.addEventListener('animationend', () => {
+      element?.classList.remove('rocketStart');
+    }, { once: true });
+  }
+
+
+
+
+}
